59e0c4e44bbc6d17cfc49216ebc71f284227ba2f
Amrith Yerramilli <amrith.yerramilli@gmail.com>
2015-10-03 17:21:53 +0530

    Initial commit - pure scaffolded app using yo angular (no SASS, yes Bootstrap)

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name evalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the evalApp
+ */
+angular.module('evalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name evalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the evalApp
+ */
+angular.module('evalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
